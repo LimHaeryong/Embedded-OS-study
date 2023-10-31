aa84a99370494fe83b606ba5d245dc857ea7ee46
LimHaeryong <nabi4622@snu.ac.kr>
2023-10-31 15:29:04 +0900

    CH10 Context switching

--- boot/Main.c
+++ boot/Main.c
@@ -6,6 +6,7 @@
 #include "HalInterrupt.h"
 #include "HalTimer.h"
 #include "task.h"
+#include "Kernel.h"
 
 static void Hw_init(void);
 static void Kernel_init(void);
@@ -32,6 +33,8 @@ void main(void)
 
     Printf_test();
     Timer_test();
+
+    Kernel_init();
     while(true);
 }
 
@@ -64,25 +67,29 @@ static void Kernel_init(void)
     {
         putstr("Task2 creation fail\n");
     }
+
+    Kernel_start();
 }
 
 static void Printf_test(void)
 {
     char* str = "printf pointer test";
     char* nullptr = 0;
-    uint32_t i = 5;
+    uint32_t i = MAX_TASK_NUM;
+    uint32_t* sysctrl0 = (uint32_t*)0x10001000;
 
     debug_printf("%s\n", "Hello printf");
     debug_printf("output string pointer: %s\n", str);
     debug_printf("%s is null pointer, %u number\n", nullptr, 10);
-    debug_printf("%u = 5\n", i);
+    debug_printf("MAX_TASK_NUM = %u\n", i);
     debug_printf("dec=%u hex=%x\n", 0xff, 0xff);
     debug_printf("print zero %u\n", 0);
+    debug_printf("SYSCTRL0 %x\n", *sysctrl0);
 }
 
 static void Timer_test(void)
 {
-    while(true)
+    for(uint32_t i = 0; i < 5; ++i)
     {
         debug_printf("current count : %u\n", Hal_timer_get_1ms_counter());
         delay(1000);
@@ -91,18 +98,30 @@ static void Timer_test(void)
 
 void User_task0(void)
 {
-    debug_printf("User Task #0\n");
-    while(true);
+    uint32_t local = 0;
+    debug_printf("User Task #0 SP=0x%x\n", &local);
+    while(true)
+    {
+        Kernel_yield();
+    }
 }
 
 void User_task1(void)
 {
-    debug_printf("User Task #1\n");
-    while(true);
+    uint32_t local = 0;
+    debug_printf("User Task #1 SP=0x%x\n", &local);
+    while(true)
+    {
+        Kernel_yield();
+    }
 }
 
 void User_task2(void)
 {
-    debug_printf("User Task #2\n");
-    while(true);
+    uint32_t local = 0;
+    debug_printf("User Task #2 SP=0x%x\n", &local);
+    while(true)
+    {
+        Kernel_yield();
+    }
 }
